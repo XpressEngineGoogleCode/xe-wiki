cb8216351f6c64827b5db52998f4ce31dd61acba
corina.udrescu@gmail.com <corina.udrescu@gmail.com@93e89afd-8622-bd68-7444-12f807e44a33>
2012-02-07 15:22:50 +0000

    Added some more headers (h4, h5, h6). Added support for lists and tables.

--- branches/modules/wiki/1.3.0/lib/wiky.inc.php
+++ branches/modules/wiki/1.3.0/lib/wiky.inc.php
@@ -101,16 +101,26 @@ class WikiSyntaxParser {
 			$text = str_replace(chr(13), '', $text);
 
 			// Replace headings
+			// ====== Heading 6 ====== 
+			$text = preg_replace("/^====== (.+?) ======( *)$/m"  , "<h6>$1</h6>", $text);
+			// ===== Heading 5 ===== 
+			$text = preg_replace("/^===== (.+?) =====( *)$/m"  , "<h5>$1</h5>", $text);
+			// ==== Heading 4 ==== 
+			$text = preg_replace("/^==== (.+?) ====( *)$/m"  , "<h4>$1</h4>", $text);
+			// === Heading 3 === 
 			$text = preg_replace("/
 									^===\s     # Line starts with three equal signs, followed by a space
 									(.+?)      # One or more characters (of any type except line breaks)
 									\s===      # Followed by another space and three equal signs
 									(\s*)$     # The line can end directly, or there can be spaces  
 								  /mx", "<h3>$1</h3>", $text); // The m modifier specifies that matches are per line, instead of per document
+			// == Heading 2 == 
 			$text = preg_replace("/^== (.+?) ==( *)$/m"  , "<h2>$1</h2>", $text);
+			// = Heading 1 =
 			$text = preg_replace("/^= (.+?) =( *)$/m"    , "<h1>$1</h1>", $text);
 			
 			// Replace code blocks
+			// {{{ This is some multiline code }}}
 			$text = preg_replace("/
 									{{{			# Starts with three braces
 									([\n]*)		# Followed by one or more newlines
@@ -118,22 +128,27 @@ class WikiSyntaxParser {
 									([\n]*)		# Followed by one or more newlines
 									}}}			# And ends with another three braces
 								 /sxe"    , "'<pre class=\'prettyprint\'>' . nl2br(htmlspecialchars(stripslashes('$2'))) . '</pre>'", $text);			
+			// `This is a short snippet of code`
 			$text = preg_replace("/`(.+?)`/"    , "<span class='inline_code'>$1</span>", $text);			
 			
 			// Replace bold			
+			// *This be bold*
 			$text = preg_replace("~
 									(?<!		# Star is not preceded
 										/)		#					by a / 
 									[*]			# Starts with star
+									([^ ])		# Must be followed by some non-space char
 									(.+?)		# Any number of characters, including whitespace (see s modifier)
 									[*]			# Ends with star
 									(?!/)		# Star isn't followed by slash 
-									~x", "<strong>$1</strong>", $text); // Bold is only replaced on the same line
+									~x", "<strong>$1$2</strong>", $text); // Bold is only replaced on the same line
 			
 			// Replace #summary
+			// #summary Summries are short descriptions of an article
 			$text = preg_replace("/^#summary(.*)/m"    , "<i>$1</i>", $text);
 			
 			// Replace links
+			// [https://my.link Click here]
 			$text = preg_replace("/
 									\[		# Starts with bracket
 									([^ ]+)  # Text without spaces
@@ -141,8 +156,49 @@ class WikiSyntaxParser {
 									(.*)	# Any character
 									\]		# Ends with bracket
 									/x"    , "<a href='$1'>$2</a>", $text);
+
+			// Unordered/Ordered list
+			// # First pass, replace entire block (ul)
+			$text = preg_replace("/
+									(\s+)	# At least one space
+									[#*]	# Star or #
+									(.+)	# Any number of characters
+									((\n)(\s+)[#*].+)*	# New line, and the above repeated any number of times
+									/x", "<ul>\n$0\n</ul>", $text);
+			// # Second pass, replace li elements
+			$text = preg_replace("/
+									(\s+)	# At least one space
+									([#*])	# A star or #
+									\s+		# At least one space
+									(.*)	# Any number of characters
+									/x", "<li>$3</li>", $text);
 			
-			
+			// Tables
+			// || 1 ||  2  ||  3 ||
+			// First pass, replace <table>
+			$text = preg_replace("/
+									^		# Start from beginning of the line
+									(		
+									  (		
+									    \n			# Newline
+										\|\|		# ||
+										.*			# Any text except |
+										\|\|		# ||
+									  )+			# One or more times
+									)				# Capture all
+									/msx", "<table border=1 cellspacing=0 cellpadding=5>\n$1\n</table>", $text);
+
+			// Second pass, replace rows (<tr>) and cells (<td>)
+			$text = preg_replace("/
+									^\|\|	# Line starts with ||
+									/mx", "<tr><td>", $text);
+			$text = preg_replace("/
+									\|\|$	# Line ends with ||
+									/mx", "</td></tr>", $text);
+			$text = preg_replace("/	
+									(\|\|)	# Any || found in text 
+									/mx", "</td><td>", $text);
+
 			// Replace new lines with paragraphs
 			$text = preg_replace("/\n(.+)\n/", '<p>$1</p>', $text);
 		}
