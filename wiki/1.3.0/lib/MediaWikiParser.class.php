42a45c3447b20b3b4b9db8784b650071d295b264
corina.udrescu <corina.udrescu@gmail.com>
2012-02-13 15:01:53 +0000

    Added MediaWiki as option on wiki backend. Added class for MediaWiki parsing and completed typefaces and escape tags. Refactored parsing classes a little to avoid duplicating code.

---
+++ wiki/1.3.0/lib/MediaWikiParser.class.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('ParserBase.class.php');
+
+class MediaWikiParser extends ParserBase {	
+	protected $typeface_symbols = array(
+		"italic" => "\'\'",
+		"bold"	=> "\'\'\'",
+		"inline_code" => "`",
+		"multiline_code_open" => "{{{",
+		"multiline_code_close" => "}}}",
+		"superscript" => '\^',
+		"subscript" => ',,',
+		"strikeout" => '~~'
+	);
+	
+	public function __construct(){
+		parent::__construct();	
+	}
+		
+	/**
+	 * Override
+	 */
+	protected function escapeWhateverThereIsToEscape(){
+		// Escape text between <nowiki> and </nowiki>
+		$this->batch_count++;
+		$this->text = preg_replace_callback("~
+												[<]nowiki[>]
+												(.*)
+												[<][/]nowiki[>]
+											~x", array($this, "_escapeBlock"), $this->text);
+
+		// Escape text right after <nowiki/>
+		$this->batch_count++;
+		$this->text = preg_replace_callback("~
+												[<]nowiki[/][>]
+												([^ ]*)
+											~x", array($this, "_escapeBlock"), $this->text);
+	}	
+	
+	private function _escapeBlock(&$matches){
+		$this->escaped_blocks[] = $matches[1];
+		return "%%%" . $this->batch_count . "%%%";
+	}
+	
+}
