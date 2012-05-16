095ecbdb2a3c82d0cd29439cf80ecf2df253ead9
corina.udrescu <corina.udrescu@gmail.com>
2012-05-16 12:21:20 +0000

    Issue 63:	Mediawiki bug: words that start with http are no longer converted to links

--- wiki/1.3.0/lib/MediaWikiParser.class.php
+++ wiki/1.3.0/lib/MediaWikiParser.class.php
@@ -238,7 +238,7 @@ class MediaWikiParser extends ParserBase
 									((https?|ftp|file)
 									://
 									[^ <]*)
-									#x", "<a href=\"$1\" title=\"$1\" class=\"external\">$1</a>", $this->text);
+									#x", "<a href=\"$2\" title=\"$2\" class=\"external\">$2</a>", $this->text);
 		// Find internal links given between [[double brackets]]
 		//	- can contain piped description [my link|description that can have many words]
 		//	- can link to local content [my link#local_anchor|and some description maybe]
