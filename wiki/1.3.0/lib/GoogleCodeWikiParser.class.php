6f8cc54bcdcfa57bb426e8bc23c26f1b22d9f201
corina.udrescu <corina.udrescu@gmail.com>
2012-06-11 14:51:57 +0000

    Fixed some issue with GoogleCode syntax: 77, 78, 81.

--- wiki/1.3.0/lib/GoogleCodeWikiParser.class.php
+++ wiki/1.3.0/lib/GoogleCodeWikiParser.class.php
@@ -64,6 +64,98 @@ class GoogleCodeWikiParser extends ParserBase
 		// = Heading 1 =
 		$this->text = preg_replace("/^= (.+?) =( *)$/m", "<h1>$1</h1>", $this->text);
 	}
+
+	/**
+	 * @brief Escapes anything the syntax needs
+	 * @developer Corina Udrescu (xe_dev@arnia.ro)
+	 * @access protected
+	 * @return
+	 */
+	function escapeWhateverThereIsToEscape()
+	{
+		// Escape <img /> tags
+		$this->text = preg_replace_callback("~
+												([<]img
+												.*
+												[/][>])
+											~x", array($this, "_escapeBlock"), $this->text);
+
+		// Escape CamelCase text square brackets' description
+		$this->text = preg_replace_callback("~
+												[\[]			# Starts with opening square bracket [
+												.*?              # Followed by anything
+												[ ]				 # Has to begin with one space - we only escape CamelCase in descriptions
+												(
+												(				# Sequence of letters that ..
+													[A-Z]	    # Start with an uppercase letter
+													[a-z0-9]+	# Followed by at least one lowercase letter
+												){2,}			# Repeated at least two times
+												)
+												.*?				# Followed by anything
+												[\]]			# Ends with closing square bracket
+											~x", array($this, "_escapeCamelCaseInSquareBracketLinks"), $this->text);
+
+		// Escape CamelCase inside external links
+		$this->text = preg_replace_callback("~
+												(https?|ftp|file)
+												://
+			                                    [^ ]*?
+												(
+												(				# Sequence of letters that ..
+													[A-Z]	    # Start with an uppercase letter
+													[a-z0-9]+	# Followed by at least one lowercase letter
+												){2,}			# Repeated at least two times
+												)
+			                                    [^ ]*?
+											~x", array($this, "_escapeCamelCaseInExternalLinks"), $this->text);
+
+
+	}
+
+	/**
+	 * @brief Callback function for escapes
+	 * @developer Corina Udrescu (xe_dev@arnia.ro)
+	 * @access private
+	 * @param $matches array()
+	 * @return string
+	 */
+	function _escapeBlock(&$matches)
+	{
+		$this->batch_count++;
+		$replacement = "%%%" . $this->batch_count . "%%%";
+		$this->escaped_blocks[$replacement] = $matches[1];
+		return $replacement;
+	}
+
+	/**
+	 * @brief Callback function for CamelCase text inside square bracket links
+	 * @developer Corina Udrescu (xe_dev@arnia.ro)
+	 * @access private
+	 * @param $matches array()
+	 * @return string
+	 */
+	function _escapeCamelCaseInSquareBracketLinks(&$matches)
+	{
+		$this->batch_count++;
+		$replacement = "%%%" . $this->batch_count . "%%%";
+		$this->escaped_blocks[$replacement] = $matches[1];
+		return str_replace($matches[1], $replacement, $matches[0]);
+	}
+
+	/**
+	 * @brief Callback function for CamelCase text inside square bracket links
+	 * @developer Corina Udrescu (xe_dev@arnia.ro)
+	 * @access private
+	 * @param $matches array()
+	 * @return string
+	 */
+	function _escapeCamelCaseInExternalLinks(&$matches)
+	{
+		$this->batch_count++;
+		$replacement =  "%%%" . $this->batch_count . "%%%";
+		$this->escaped_blocks[$replacement] = $matches[2];
+		return str_replace($matches[2], $replacement, $matches[0]);
+	}
 }
 /* End of file GoogleCodeWikiParser.class.php */
 /* Location: GoogleCodeWikiParser.class.php */
