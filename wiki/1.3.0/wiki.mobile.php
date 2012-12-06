73bc4646d5764e0b95e0becb10bea63b63dfbe2e
corina.udrescu <corina.udrescu@gmail.com>
2012-12-06 14:14:10 +0000

    Imporved code comments for wiki backend, mobile and wap classes.

--- wiki/1.3.0/wiki.mobile.php
+++ wiki/1.3.0/wiki.mobile.php
@@ -2,53 +2,66 @@
 
 require_once(_XE_PATH_.'modules/wiki/wiki.view.php');
 
+/**
+ * @class  wikiMobile
+ * @developer NHN (developers@xpressengine.com)
+ *
+ * Wiki mobile class
+ */
 class wikiMobile extends wikiView {
-    function init()
-    {
-	if($this->module_info->list_count)
-	{
-	    $this->list_count = $this->module_info->list_count;
-	}
-	if($this->module_info->search_list_count)
-	{
-	    $this->search_list_count = $this->module_info->search_list_count;
-	}
-	if($this->module_info->page_count)
-	{
-	    $this->page_count = $this->module_info->page_count;
-	}
-	$this->except_notice = $this->module_info->except_notice == 'N' ? false : true;
 
 	/**
-	* Check consultation stats.
-	* If the current user is not logged the Writing / Comment / List / View grants will be removes
-	**/
-	if($this->module_info->consultation == 'Y' && !$this->grant->manager) 
-	{
-	    $this->consultation = true; 
-	    if(!Context::get('is_logged'))
-	    {
-			$this->grant->list = $this->grant->write_document = $this->grant->write_comment = $this->grant->view = false;
-	    }
-	} 
-	else 
-	{
-	    $this->consultation = false;
-	}
+	 * Initialize mobile views
+	 */
+	function init()
+    {
+		if($this->module_info->list_count)
+		{
+			$this->list_count = $this->module_info->list_count;
+		}
+		if($this->module_info->search_list_count)
+		{
+			$this->search_list_count = $this->module_info->search_list_count;
+		}
+		if($this->module_info->page_count)
+		{
+			$this->page_count = $this->module_info->page_count;
+		}
+		$this->except_notice = $this->module_info->except_notice == 'N' ? FALSE : TRUE;
 
-	$oDocumentModel = &getModel('document');
-	$extra_keys = $oDocumentModel->getExtraKeys($this->module_info->module_srl);
-	Context::set('extra_keys', $extra_keys);
+		/**
+		* Check consultation stats.
+		* If the current user is not logged the Writing / Comment / List / View grants will be removes
+		**/
+		if($this->module_info->consultation == 'Y' && !$this->grant->manager)
+		{
+			$this->consultation = TRUE;
+			if(!Context::get('is_logged'))
+			{
+				$this->grant->list = $this->grant->write_document = $this->grant->write_comment = $this->grant->view = FALSE;
+			}
+		}
+		else
+		{
+			$this->consultation = FALSE;
+		}
 
-	$template_path = sprintf("%sm.skins/%s/",$this->module_path, $this->module_info->mskin);
-	if(!is_dir($template_path)||!$this->module_info->mskin) 
-	{
-	    $this->module_info->mskin = 'default';
-	    $template_path = sprintf("%sm.skins/%s/",$this->module_path, $this->module_info->mskin);
-	}
-	$this->setTemplatePath($template_path);
+		$oDocumentModel = &getModel('document');
+		$extra_keys = $oDocumentModel->getExtraKeys($this->module_info->module_srl);
+		Context::set('extra_keys', $extra_keys);
+
+		$template_path = sprintf("%sm.skins/%s/",$this->module_path, $this->module_info->mskin);
+		if(!is_dir($template_path)||!$this->module_info->mskin)
+		{
+			$this->module_info->mskin = 'default';
+			$template_path = sprintf("%sm.skins/%s/",$this->module_path, $this->module_info->mskin);
+		}
+		$this->setTemplatePath($template_path);
     }
 
+	/**
+	 * Displays a mobile ready view of a document's comments
+	 */
 	function displayWikiCommentList(){
 		$document_srl = Context::get('document_srl');
 		$oDocumentModel = &getModel('document');
@@ -62,6 +75,9 @@ class wikiMobile extends wikiView {
 		$this->setTemplateFile('comment');
 	}
 
+	/**
+	 * Displays a mobile ready view for adding a new comment
+	 */
 	function dispWikiAddComment() {
 		$document_srl = Context::get('document_srl');
 		$oDocumentModel = &getModel('document');
